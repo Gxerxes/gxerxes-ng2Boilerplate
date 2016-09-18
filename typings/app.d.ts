0b902e30bc0a394bfd25fa5375115bc94e42985e
root <born2net@gmail.com>
2016-09-17 21:05:19 -0700

    v1

--- typings/app.d.ts
+++ typings/app.d.ts
@@ -4,28 +4,24 @@
  * to users.
  */
 
-
 /** used **/
 ///<reference path="../typings/browser/ambient/redux-thunk/redux-thunk.d.ts" />
 ///<reference path="../typings/browser/ambient/redux/redux.d.ts" />
 ///<reference path="../typings/browser/ambient/reselect/reselect.d.ts" />
+///<reference path="../typings/jquery/jquery.d.ts" />
+///<reference path="../typings/bootbox/bootbox.d.ts" />
 
 /** not used **/
 //<reference path="../typings/browser/ambient/require/require.d.ts" />
-///<reference path="../typings/jquery/jquery.d.ts" />
 //<reference path='../node_modules/angular2/typings/browser.d.ts'/>
 //<reference path="../typings/browser/ambient/systemjs/systemjs.d.ts" />
 //<reference path="../typings/browser/ambient/es6-shim/es6-shim.d.ts" />
-///<reference path="../typings/bootbox/bootbox.d.ts" />
-// <reference path="../typings/hammerjs/hammerjs.d.ts"/>
-// <reference path="../typings/jasmine/jasmine.d.ts"/>
-// <reference path="../typings/angular-protractor/angular-protractor.d.ts"/>
-// <reference path="../typings/node/node.d.ts" />
-// <reference path="../typings/es6-shim/es6-shim.d.ts" />
-// <reference path="../typings/zone.js/zone.js.d.ts"/>
-
-
-
+//<reference path="../typings/hammerjs/hammerjs.d.ts"/>
+//<reference path="../typings/jasmine/jasmine.d.ts"/>
+//<reference path="../typings/angular-protractor/angular-protractor.d.ts"/>
+//<reference path="../typings/node/node.d.ts" />
+//<reference path="../typings/es6-shim/es6-shim.d.ts" />
+//<reference path="../typings/zone.js/zone.js.d.ts"/>
 
 declare var assert:any;
 // declare var module:any;
